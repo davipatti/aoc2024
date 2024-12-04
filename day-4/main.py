13667567a8782c2a677827604edf2d28b545edf5
David Pattinson <davipatti@protonmail.com>
2024-12-04 09:29:23 -0500

    Day 4

---
+++ day-4/main.py
@@ -0,0 +1,68 @@
+# %%
+
+import numpy as np
+
+# %%
+
+with open("input") as fobj:
+    grid = np.array([tuple(line.strip()) for line in fobj])
+
+# %% part 1
+
+
+def rotations(grid):
+    for _ in range(4):
+        yield grid
+        grid = np.rot90(grid)
+
+
+def diagonals(grid):
+    nrows, ncols = grid.shape
+    assert nrows == ncols
+    for offset in range(-(nrows - 1), nrows):
+        yield np.diagonal(grid, offset)
+
+
+def part1(grid):
+
+    total = 0
+
+    for rotation in rotations(grid):
+        for row in rotation:
+            total += "".join(row).count("XMAS")
+
+        for diagonal in diagonals(rotation):
+            total += "".join(diagonal).count("XMAS")
+
+    return total
+
+
+print(part1(grid))
+
+
+# %% part 2
+
+
+def part2(grid):
+
+    # X-shaped mask
+    row_mask = np.array([0, 0, 1, 2, 2])
+    col_mask = np.array([0, 2, 1, 0, 2])
+
+    total = 0
+
+    # Slide the X-shaped mask over all positions in all rotations of the grid
+    nrows, ncols = grid.shape
+    for rotation in rotations(grid):
+        for row_shift in range(nrows - 2):
+            for col_shift in range(ncols - 2):
+
+                i = row_mask + row_shift
+                j = col_mask + col_shift
+
+                total += "".join(rotation[i, j]).count("MSAMS")
+
+    return total
+
+
+print(part2(grid))
